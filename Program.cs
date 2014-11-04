48c99483b6ca6e30e0f5d68c4dcd7ffd0e42a7f8
rovepe17 <rociounipamplona@hotmail.com>
2014-11-04 16:15:55 -0500

    Appswitch

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero;
+            Console.WriteLine("programa sentencia switch");
+            Console.WriteLine("los numeros");
+            numero = Console.ReadLine();
+            switch (numero.ToUpper())
+            {
+                case "UNO":
+                Console.WriteLine("1");
+                break;
+                case "DOS":
+                Console.WriteLine("2");
+                break;
+                case "TRES":
+                Console.WriteLine("3");
+                break;
+                case "CUATRO":
+                Console.WriteLine("4");
+                break;
+                case "CINCO":
+                Console.WriteLine("5");
+                break;
+                default:
+                Console.WriteLine(" numero fuera de rango:");
+                break;
+            }
+
+                    Console.ReadKey();
+        }
+    }
+}
